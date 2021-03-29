a0054abb87c4111fb6e3f40400334760fd151e26
Chris Spohn <chris.j.spohn@gmail.com>
2021-03-29 15:33:42 -0600

    Render styled header for StyleSurvey
    Co-authored-by: Kristen Bair <69478485+kristenmb@users.noreply.github.com >
    Co-authored-by: Connie Hong <67291333+conconartist@users.noreply.github.com>

--- code/web/src/modules/user/StyleSurvey.js
+++ code/web/src/modules/user/StyleSurvey.js
@@ -1,6 +1,18 @@
 import React, { PureComponent } from 'react'
 
+import PropTypes from 'prop-types'
+import { connect } from 'react-redux'
+import { Helmet } from 'react-helmet'
+
+// UI Imports
 import Card from '../../ui/card/Card'
+import Button from '../../ui/button/Button'
+import H4 from '../../ui/typography/H4'
+import { white, grey, grey2, black } from '../../ui/common/colors'
+
+// UI Imports
+import { Grid, GridCell } from '../../ui/grid'
+import { H3 } from '../../ui/typography'
 
 class StyleSurvey extends PureComponent {
   constructor(props) {
@@ -13,7 +25,17 @@ class StyleSurvey extends PureComponent {
 
   render() {
     return (
-      <h1>Survey Page</h1>
+      <>
+      {/* Top title bar */}
+      <Grid style={{ backgroundColor: grey }}>
+        <GridCell style={{ padding: '2em', textAlign: 'center' }}>
+          <H3 font="secondary">Style Survey!</H3>
+
+          <p style={{ marginTop: '1em', color: grey2 }}>Let's find your style profile!</p>
+        </GridCell>
+      </Grid>
+
+      </>
     )
   }
 }
