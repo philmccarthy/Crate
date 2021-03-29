6176e0f39a965524760387548700afc05204d03a
Chris Spohn <chris.j.spohn@gmail.com>
2021-03-29 14:57:12 -0600

    Built our simple StyleSurvey component
    Co-authored-by: Kristen Bair <69478485+kristenmb@users.noreply.github.com >
    Co-authored-by: Connie Hong <67291333+conconartist@users.noreply.github.com>

--- code/web/src/modules/user/StyleSurvey.js
+++ code/web/src/modules/user/StyleSurvey.js
@@ -1,1 +1,21 @@
-import React from 'react';
+import React, { PureComponent } from 'react'
+
+import Card from '../../ui/card/Card'
+
+class StyleSurvey extends PureComponent {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      result: ''
+    }
+  }
+
+  render() {
+    return (
+      <h1>Survey Page</h1>
+    )
+  }
+}
+
+export default StyleSurvey;
